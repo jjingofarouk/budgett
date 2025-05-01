c84042edfae1324287e2ddbdb3d3a3ca352a48d6
jjingofarouk <jjingofaroukk@gmail.com>
2025-05-01 23:09:59 +0300

    Update ExpenseItem.js

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,1 +1,34 @@
+// components/ExpenseItem.js
+import React, { useContext } from 'react';
+import { AppContext } from '../context/AppContext';
+import { Button } from 'react-bootstrap';
+import { FaTrashAlt } from 'react-icons/fa';
 
+const ExpenseItem = (props) => {
+    const { dispatch, currency } = useContext(AppContext);
+
+    const handleDeleteExpense = () => {
+        dispatch({
+            type: 'DELETE_EXPENSE',
+            payload: props.id,
+        });
+    };
+
+    return (
+        <tr>
+            <td>{props.name}</td>
+            <td>{currency}{props.cost.toLocaleString()}</td>
+            <td>
+                <Button
+                    variant="danger"
+                    size="sm"
+                    onClick={handleDeleteExpense}
+                >
+                    <FaTrashAlt />
+                </Button>
+            </td>
+        </tr>
+    );
+};
+
+export default ExpenseItem;
