19ed78eeb70c61a7055911322cca29f62ff5aa00
jjingofarouk <jjingofaroukk@gmail.com>
2025-05-01 23:09:42 +0300

    Update ExpenseList.js

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,1 +1,33 @@
+// components/ExpenseList.js
+import React, { useContext } from 'react';
+import { AppContext } from '../context/AppContext';
+import ExpenseItem from './ExpenseItem';
+import { Table } from 'react-bootstrap';
 
+const ExpenseList = () => {
+    const { expenses } = useContext(AppContext);
+
+    return (
+        <Table striped bordered hover>
+            <thead>
+                <tr>
+                    <th>Department</th>
+                    <th>Allocated Budget</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                {expenses.map((expense) => (
+                    <ExpenseItem
+                        key={expense.id}
+                        id={expense.id}
+                        name={expense.name}
+                        cost={expense.cost}
+                    />
+                ))}
+            </tbody>
+        </Table>
+    );
+};
+
+export default ExpenseList;
