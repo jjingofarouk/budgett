18d34f9e59364ce87f62b7106b9ff781291736e1
jjingofarouk <jjingofaroukk@gmail.com>
2025-05-01 23:19:53 +0300

    Update Remaining.js

--- src/components/Remaining.js
+++ src/components/Remaining.js
@@ -2,15 +2,16 @@
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 import { Card, Alert } from 'react-bootstrap';
+import { FaChartLine } from 'react-icons/fa';
 
 const Remaining = () => {
     const { remaining, currency, budget } = useContext(AppContext);
     const alertType = remaining < 0 ? 'danger' : remaining <= budget * 0.1 ? 'warning' : 'success';
 
     return (
-        <Card className='p-3'>
+        <Card className='p-4'>
             <Alert variant={alertType}>
-                Remaining: {currency}{remaining.toLocaleString()}
+                <FaChartLine className="me-2" /> Liquidity Reserve: {currency}{remaining.toLocaleString()}
             </Alert>
         </Card>
     );
