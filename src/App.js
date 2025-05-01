9d272b299a0ee0dd3cda2d9c83b8e66d7b7b6548
jjingofarouk <jjingofaroukk@gmail.com>
2025-05-01 23:22:13 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,45 +10,66 @@ import { AppProvider } from './context/AppContext';
 import CurrencySelector from './components/CurrencySelector';
 import BudgetChart from './components/BudgetChart';
 import ExpenseHistory from './components/ExpenseHistory';
+import BudgetForecast from './components/BudgetForecast';
+import CollaborationPanel from './components/CollaborationPanel';
+import AIInsights from './components/AIInsights';
 import './App.css';
 
 const App = () => {
     return (
         <AppProvider>
             <div className='container'>
-                <h1 className='mt-3'>Company's Budget Allocation</h1>
+                <header className='header'>
+                    <img src="/logo.png" alt="VaultVision Logo" className="logo" />
+                    <h1>VaultVision: Enterprise Wealth Architect</h1>
+                </header>
                 <CurrencySelector />
-                <div className='row mt-3'>
-                    <div className='col-sm'>
+                <div className='row mt-3 dashboard-grid'>
+                    <div className='col-md-4'>
                         <Budget />
                     </div>
-                    <div className='col-sm'>
+                    <div className='col-md-4'>
                         <Remaining />
                     </div>
-                    <div className='col-sm'>
+                    <div className='col-md-4'>
                         <ExpenseTotal />
                     </div>
                 </div>
                 <div className='row mt-3'>
-                    <div className='col-sm'>
+                    <div className='col-md-6'>
                         <BudgetChart />
                     </div>
+                    <div className='col-md-6'>
+                        <BudgetForecast />
+                    </div>
                 </div>
-                <h3 className='mt-3'>Expenses</h3>
+                <h3 className='mt-3'>Strategic Allocations</h3>
                 <div className='row mt-3'>
-                    <div className='col-sm'>
+                    <div className='col-12'>
                         <ExpenseList />
                     </div>
                 </div>
-                <h3 className='mt-3'>Add/Modify Allocation</h3>
+                <h3 className='mt-3'>Resource Allocation</h3>
                 <div className='row mt-3'>
-                    <div className='col-sm'>
+                    <div className='col-12'>
                         <AllocationForm />
                     </div>
                 </div>
-                <h3 className='mt-3'>Expense History</h3>
+                <h3 className='mt-3'>AI-Powered Insights</h3>
                 <div className='row mt-3'>
-                    <div className='col-sm'>
+                    <div className='col-12'>
+                        <AIInsights />
+                    </div>
+                </div>
+                <h3 className='mt-3'>Collaboration Hub</h3>
+                <div className='row mt-3'>
+                    <div className='col-12'>
+                        <CollaborationPanel />
+                    </div>
+                </div>
+                <h3 className='mt-3'>Transaction Ledger</h3>
+                <div className='row mt-3'>
+                    <div className='col-12'>
                         <ExpenseHistory />
                     </div>
                 </div>
