5c8348c8825817a91526040373955662e13e5a62
jjingofarouk <jjingofaroukk@gmail.com>
2025-05-01 23:29:58 +0300

    Update ExpenseTotal.js

--- src/components/ExpenseTotal.js
+++ src/components/ExpenseTotal.js
@@ -1,15 +1,20 @@
 // components/ExpenseTotal.js
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
-import { Card } from 'react-bootstrap';
+import { Card, Badge } from 'react-bootstrap';
+import { FaMoneyCheckAlt } from 'react-icons/fa';
 
 const ExpenseTotal = () => {
-    const { expenses, currency } = useContext(AppContext);
+    const { expenses, currency, budget } = useContext(AppContext);
     const totalExpenses = expenses.reduce((total, item) => total + item.cost, 0);
+    const percentage = ((totalExpenses / budget) * 100).toFixed(1);
 
     return (
-        <Card className='p-3'>
-            <h4>Spent: {currency}{totalExpenses.toLocaleString()}</h4>
+        <Card className='p-4'>
+            <h4><FaMoneyCheckAlt className="me-2" /> Deployed Capital: {currency}{totalExpenses.toLocaleString()}</h4>
+            <Badge bg={percentage > 80 ? 'warning' : 'info'} className="mt-2">
+                {percentage}% of Budget
+            </Badge>
         </Card>
     );
 };
