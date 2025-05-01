33be4f9a718762902be456cfc7629d8d8226eb80
jjingofarouk <jjingofaroukk@gmail.com>
2025-05-01 23:09:29 +0300

    Update ExpenseTotal.js

--- src/components/ExpenseTotal.js
+++ src/components/ExpenseTotal.js
@@ -1,1 +1,17 @@
+// components/ExpenseTotal.js
+import React, { useContext } from 'react';
+import { AppContext } from '../context/AppContext';
+import { Card } from 'react-bootstrap';
 
+const ExpenseTotal = () => {
+    const { expenses, currency } = useContext(AppContext);
+    const totalExpenses = expenses.reduce((total, item) => total + item.cost, 0);
+
+    return (
+        <Card className='p-3'>
+            <h4>Spent: {currency}{totalExpenses.toLocaleString()}</h4>
+        </Card>
+    );
+};
+
+export default ExpenseTotal;
