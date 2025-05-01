f00837d3fbd8967e03ebee1434c91280c05d418d
jjingofarouk <jjingofaroukk@gmail.com>
2025-05-01 23:09:12 +0300

    Update Remaining.js

--- src/components/Remaining.js
+++ src/components/Remaining.js
@@ -1,1 +1,19 @@
+// components/Remaining.js
+import React, { useContext } from 'react';
+import { AppContext } from '../context/AppContext';
+import { Card, Alert } from 'react-bootstrap';
 
+const Remaining = () => {
+    const { remaining, currency, budget } = useContext(AppContext);
+    const alertType = remaining < 0 ? 'danger' : remaining <= budget * 0.1 ? 'warning' : 'success';
+
+    return (
+        <Card className='p-3'>
+            <Alert variant={alertType}>
+                Remaining: {currency}{remaining.toLocaleString()}
+            </Alert>
+        </Card>
+    );
+};
+
+export default Remaining;
