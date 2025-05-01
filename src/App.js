224e1148f80ea03a39ab0d64bf5ff04002b7908a
jjingofarouk <jjingofaroukk@gmail.com>
2025-05-01 23:07:20 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,47 +1,60 @@
+// App.js
 import React from 'react';
-
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-//Code to import Budget.js
 import Budget from './components/Budget';
-
-// Add code to import the other components here under
-
-
+import Remaining from './components/Remaining';
+import ExpenseTotal from './components/ExpenseTotal';
+import ExpenseList from './components/ExpenseList';
+import AllocationForm from './components/AllocationForm';
 import { AppProvider } from './context/AppContext';
+import CurrencySelector from './components/CurrencySelector';
+import BudgetChart from './components/BudgetChart';
+import ExpenseHistory from './components/ExpenseHistory';
+import './App.css';
+
 const App = () => {
     return (
         <AppProvider>
             <div className='container'>
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
-                    <div className='row mt-3'>
-                        {
-                            /* Add Budget component here */
-                        }        
-
-                        {
-                            /* Add Remaining component here*/
-                        }        
-
-                        {
-                            /* Add ExpenseTotal component here */
-                        }        
-                       
-                        {
-                            /* Add ExpenseList component here */
-                        }         
-
-                        {
-                            /* Add ExpenseItem component here */
-                        }        
-
-                        {
-                            /* Add AllocationForm component here under */
-                        }        
-
+                <CurrencySelector />
+                <div className='row mt-3'>
+                    <div className='col-sm'>
+                        <Budget />
+                    </div>
+                    <div className='col-sm'>
+                        <Remaining />
+                    </div>
+                    <div className='col-sm'>
+                        <ExpenseTotal />
+                    </div>
+                </div>
+                <div className='row mt-3'>
+                    <div className='col-sm'>
+                        <BudgetChart />
+                    </div>
+                </div>
+                <h3 className='mt-3'>Expenses</h3>
+                <div className='row mt-3'>
+                    <div className='col-sm'>
+                        <ExpenseList />
+                    </div>
+                </div>
+                <h3 className='mt-3'>Add/Modify Allocation</h3>
+                <div className='row mt-3'>
+                    <div className='col-sm'>
+                        <AllocationForm />
+                    </div>
+                </div>
+                <h3 className='mt-3'>Expense History</h3>
+                <div className='row mt-3'>
+                    <div className='col-sm'>
+                        <ExpenseHistory />
+                    </div>
                 </div>
             </div>
         </AppProvider>
     );
 };
+
 export default App;
